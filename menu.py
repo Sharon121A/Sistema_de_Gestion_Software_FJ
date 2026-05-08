c66f0d8b97426a0f11a0752d92aa850695fc31ee
Sharon121A <ayalalorena224@gmail.com>
2026-05-08 18:56:30 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,7 @@
+def ejecutar_pruebas():
+    """Ejecuta las pruebas o inicia el menú principal del programa."""
+    print("Ejecutando pruebas...")
+    # TODO: implementar la lógica de pruebas aquí
+
+if __name__ == "__main__":
+    ejecutar_pruebas()
