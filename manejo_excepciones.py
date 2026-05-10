07ff752b366363ae4f5f476e8e3db84391babd49
andrespipe77 <amaolugo2202@gmail.com>
2026-05-10 07:21:26 +0200

    Update manejo_excepciones.py

--- manejo_excepciones.py
+++ manejo_excepciones.py
@@ -6,17 +6,18 @@ class SistemaError(Exception):
     """Excepción base del sistema"""
     pass
 
-
 class DatosInvalidosError(SistemaError):
     """Error por datos incorrectos"""
     pass
-
 
 class ServicioNoDisponibleError(SistemaError):
     """Error cuando un servicio no está disponible"""
     pass
 
-
 class ReservaError(SistemaError):
     """Error relacionado con reservas"""
+    pass
+
+class CalculoError(SistemaError):
+    """Error en cálculos inconsistentes"""
     pass
